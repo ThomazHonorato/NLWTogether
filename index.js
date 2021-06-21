ecc6f872779848d297bac351bca6336a83227335
Thomaz Honorato <thomaz.honorato@gmail.com>
2021-06-20 22:21:46 -0300

    Reworked files into repository

---
+++ index.js
@@ -0,0 +1,4 @@
+function enviarEmail(nome, email) {
+    console.log("Enviar Email para: " + nome + " com email: " + email);
+}
+enviarEmail("Thomaz", "thomaz.honorato@gmail.com");
---
+++ index.ts
@@ -0,0 +1,5 @@
+function enviarEmail(nome: String, email: String) {
+    console.log(`Enviar Email para: ${nome} com email: ${email}`);
+}
+
+enviarEmail("Thomaz", "thomaz.honorato@gmail.com");
