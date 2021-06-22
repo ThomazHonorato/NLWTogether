bd4dfce339d2cc6443a83bb4fe2d279abda05962
Thomaz Honorato <thomaz.honorato@gmail.com>
2021-06-22 09:10:26 -0300

    Iniciado e configurado banco de dados sqlite 3 e iniciado a primeira tabela a de usuarios

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,8 @@
 import express from "express";
-
+import "reflect-metadata";
+import "./database";
 const app = express();
 
-app.get("/", (request, response) => {
-    return response.json({ "message": "Olá Mundo" })
-});
-
-app.post("/testpost", (request, response) => {
-    return response.json({ "message": "Olá NLW Método Post" });
-})
 
 app.listen(3000, () => {
     console.log("Server is running!");
